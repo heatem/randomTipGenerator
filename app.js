f8a4f2489093d4295ad752c7ab0fcc39ed4cc5c8
Heather Mason <heatem@gmail.com>
2020-10-10 19:13:37 -0400

    create project

---
+++ app.js
@@ -0,0 +1,25 @@
+let tips = [
+    {
+        tipText: "Join 757ColorCoded on Slack!",
+        link: {
+            url: "https://757colorcoded.slack.com/",
+            display: "757ColorCoded on Slack",
+        },
+        contributor: "Heather"
+    },
+    {
+        tipText: "Join 757ColorCoded on Meetup",
+        link: {
+            url: "https://meetup.com/757colorcoded",
+        },
+    }
+    // TODO: Add more tips
+]
+
+function generateTip() {
+    // TODO: Add a constant called randomTip that is a random integer between 0 and the length of the tips array
+    // TODO: Using randomTip, show the tipText in the paragraph with the tip ID using the random integer
+    // TODO: Using randomTip, if there is a link url, add it to the paragraph with the link ID. The href should be set to the url. Show the link description if it exists, otherwise show the url
+    // TODO: Clear the link paragraph if the tip doesn't include link data
+    // TODO: Using randomTip, show the contributor in the paragraph with the contributor ID
+}
